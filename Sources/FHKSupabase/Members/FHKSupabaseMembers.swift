0681226c0341c8568d729ec640b526adcfe6e727
Fredy Leon <fredy.leon@ua.la.com>
2026-04-25 08:57:03 +0200

    test print client supabase

--- Sources/FHKSupabase/Members/FHKSupabaseMembers.swift
+++ Sources/FHKSupabase/Members/FHKSupabaseMembers.swift
@@ -40,6 +40,7 @@ public final class FHKSupabaseMembers: FHKSupabaseErrorProtocol, FHKSupabaseMemb
     }
     
     public func fetchFamilyMembers(parentEmail: String) async throws -> [MemberEntity] {
+        print(supabaseClient)
         let members: [MemberDto] = try await supabaseClient
             .from(DB.TABLE_FAMILY_MEMBER.NAME)
             .select()
